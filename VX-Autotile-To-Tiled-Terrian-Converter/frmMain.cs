539227b573d08d4ecbdacfa4c145187ad5521b32
alloin <antony@alloin.org>
2020-03-06 20:18:37 +0100

    added inverse checkbox
    added checkbox to convert tiled to vx

--- VX-Autotile-To-Tiled-Terrian-Converter/frmMain.cs
+++ VX-Autotile-To-Tiled-Terrian-Converter/frmMain.cs
@@ -65,9 +65,19 @@ namespace Multi_Purpose_Graphic_Splitter
             var x = (int)Math.Floor(e.X / 32f);
             var y = (int)Math.Floor(e.Y / 32f);
             selectRect = Rectangle.Empty;
-            if (x >= 0 && y >= 0 && (x * 32 + 64) <= pnlSampleImg.Width && (y * 32 + 96) <= pnlSampleImg.Height)
+            if (inverseBox.Checked)
             {
-                selectRect = new Rectangle(x * 32, y * 32, 64, 96);
+                if (x >= 0 && y >= 0 && (x * 32 + 128) <= pnlSampleImg.Width && (y * 32 + 64) <= pnlSampleImg.Height)
+                {
+                    selectRect = new Rectangle(x * 32, y * 32, 128, 64);
+                }
+            }
+            else
+            {
+                if (x >= 0 && y >= 0 && (x * 32 + 64) <= pnlSampleImg.Width && (y * 32 + 96) <= pnlSampleImg.Height)
+                {
+                    selectRect = new Rectangle(x * 32, y * 32, 64, 96);
+                }
             }
             UpdatePreview();
         }
@@ -78,44 +88,85 @@ namespace Multi_Purpose_Graphic_Splitter
             var y = (int)Math.Floor(e.Y / 32f);
             if (selectRect != Rectangle.Empty && selectRect.X == x * 32 && selectRect.Y == y * 32)
             {
-                var b = new Bitmap(128, 64);
-                var g = Graphics.FromImage(b);
-                g.Clear(Color.Transparent);
-
-
-                //Top Left
-                g.DrawImage(sampleImg, new Rectangle(0, 0, 16, 16), new Rectangle(selectRect.X + 48, selectRect.Y + 16, 16,16), GraphicsUnit.Pixel);
-                //Left
-                g.DrawImage(sampleImg, new Rectangle(0, 16, 16, 32), new Rectangle(selectRect.X + 48, selectRect.Y + 48, 16, 32), GraphicsUnit.Pixel);
-                //Bottom Left
-                g.DrawImage(sampleImg, new Rectangle(0, 48, 16, 16), new Rectangle(selectRect.X + 48, selectRect.Y + 0, 16, 16), GraphicsUnit.Pixel);
-                //Top
-                g.DrawImage(sampleImg, new Rectangle(16, 0, 32, 16), new Rectangle(selectRect.X + 16, selectRect.Y + 80, 32, 16), GraphicsUnit.Pixel);
-                //Bottom
-                g.DrawImage(sampleImg, new Rectangle(16, 48, 32, 16), new Rectangle(selectRect.X + 16, selectRect.Y + 32, 32, 16), GraphicsUnit.Pixel);
-                //Top Right
-                g.DrawImage(sampleImg, new Rectangle(48, 0, 16, 16), new Rectangle(selectRect.X + 32, selectRect.Y + 16, 16, 16), GraphicsUnit.Pixel);
-                //Right
-                g.DrawImage(sampleImg, new Rectangle(48, 16, 16, 32), new Rectangle(selectRect.X + 0, selectRect.Y + 48, 16, 32), GraphicsUnit.Pixel);
-                //Bottom Right
-                g.DrawImage(sampleImg, new Rectangle(48, 48, 16, 16), new Rectangle(selectRect.X + 32, selectRect.Y + 0, 16, 16), GraphicsUnit.Pixel);
-
-
-
-                //Right Chunk
-                g.DrawImage(sampleImg, new Rectangle(64, 0, 64, 64), new Rectangle(selectRect.X, selectRect.Y + 32, 64, 64), GraphicsUnit.Pixel);
-
-                g.Dispose();
-
-                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
-                saveFileDialog1.Filter = "Png Image|*.png";
-                saveFileDialog1.Title = "Save Terrain Tile";
-                saveFileDialog1.ShowDialog();
-
-                // If the file name is not an empty string open it for saving.  
-                if (saveFileDialog1.FileName != "")
+                if (inverseBox.Checked)
                 {
-                    b.Save(saveFileDialog1.FileName,ImageFormat.Png);
+                    var b = new Bitmap(64, 96);
+                    var g = Graphics.FromImage(b);
+                    g.Clear(Color.Transparent);
+                    // python left, upper, right, lower
+                    // c# x, y, width, height
+                    g.DrawImage(sampleImg, new Rectangle(48, 16, 16, 16), new Rectangle(selectRect.X + 0, selectRect.Y + 0, 16, 16), GraphicsUnit.Pixel);
+                    g.DrawImage(sampleImg, new Rectangle(32, 16, 16, 16), new Rectangle(selectRect.X + 48, selectRect.Y + 0, 16, 16), GraphicsUnit.Pixel);
+                    g.DrawImage(sampleImg, new Rectangle(48, 0, 16, 16), new Rectangle(selectRect.X + 0, selectRect.Y + 48, 16, 16), GraphicsUnit.Pixel);
+                    g.DrawImage(sampleImg, new Rectangle(32, 0, 16, 16), new Rectangle(selectRect.X + 48, selectRect.Y + 48, 16, 16), GraphicsUnit.Pixel);
+
+                    g.DrawImage(sampleImg, new Rectangle(0, 0, 16, 16), new Rectangle(selectRect.X + 64, selectRect.Y + 0, 16, 16), GraphicsUnit.Pixel);
+                    g.DrawImage(sampleImg, new Rectangle(16, 0, 16, 16), new Rectangle(selectRect.X + 112, selectRect.Y + 0, 16, 16), GraphicsUnit.Pixel);
+                    g.DrawImage(sampleImg, new Rectangle(0, 16, 16, 16), new Rectangle(selectRect.X + 64, selectRect.Y + 48, 16, 16), GraphicsUnit.Pixel);
+                    g.DrawImage(sampleImg, new Rectangle(16, 16, 16, 16), new Rectangle(selectRect.X + 112, selectRect.Y + 48, 16, 16), GraphicsUnit.Pixel);
+
+                    g.DrawImage(sampleImg, new Rectangle(0, 32, 16, 16), new Rectangle(selectRect.X + 64, selectRect.Y + 0, 16, 16), GraphicsUnit.Pixel);
+                    g.DrawImage(sampleImg, new Rectangle(48, 32, 16, 16), new Rectangle(selectRect.X + 112, selectRect.Y + 0, 16, 16), GraphicsUnit.Pixel);
+                    g.DrawImage(sampleImg, new Rectangle(16, 32, 32, 16), new Rectangle(selectRect.X + 80, selectRect.Y + 0, 32, 16), GraphicsUnit.Pixel);
+                    g.DrawImage(sampleImg, new Rectangle(0, 48, 16, 32), new Rectangle(selectRect.X + 64, selectRect.Y + 16, 16, 32), GraphicsUnit.Pixel);
+
+                    g.DrawImage(sampleImg, new Rectangle(48, 48, 16, 32), new Rectangle(selectRect.X + 112, selectRect.Y + 16, 16, 32), GraphicsUnit.Pixel);
+                    g.DrawImage(sampleImg, new Rectangle(0, 80, 16, 16), new Rectangle(selectRect.X + 64, selectRect.Y + 48, 16, 16), GraphicsUnit.Pixel);
+                    g.DrawImage(sampleImg, new Rectangle(48, 80, 16, 16), new Rectangle(selectRect.X + 112, selectRect.Y + 48, 16, 16), GraphicsUnit.Pixel);
+                    g.DrawImage(sampleImg, new Rectangle(16, 80, 32, 16), new Rectangle(selectRect.X + 80, selectRect.Y + 48, 32, 16), GraphicsUnit.Pixel);
+
+                    g.DrawImage(sampleImg, new Rectangle(16, 48, 32, 32), new Rectangle(selectRect.X + 80, selectRect.Y + 16, 32, 32), GraphicsUnit.Pixel);
+
+                    SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                    saveFileDialog1.Filter = "Png Image|*.png";
+                    saveFileDialog1.Title = "Save Terrain Tile";
+                    saveFileDialog1.ShowDialog();
+
+                    // If the file name is not an empty string open it for saving.  
+                    if (saveFileDialog1.FileName != "")
+                    {
+                        b.Save(saveFileDialog1.FileName, ImageFormat.Png);
+                    }
+                } else
+                {
+                    var b = new Bitmap(128, 64);
+                    var g = Graphics.FromImage(b);
+                    g.Clear(Color.Transparent);
+
+
+                    //Top Left
+                    g.DrawImage(sampleImg, new Rectangle(0, 0, 16, 16), new Rectangle(selectRect.X + 48, selectRect.Y + 16, 16, 16), GraphicsUnit.Pixel);
+                    //Left
+                    g.DrawImage(sampleImg, new Rectangle(0, 16, 16, 32), new Rectangle(selectRect.X + 48, selectRect.Y + 48, 16, 32), GraphicsUnit.Pixel);
+                    //Bottom Left
+                    g.DrawImage(sampleImg, new Rectangle(0, 48, 16, 16), new Rectangle(selectRect.X + 48, selectRect.Y + 0, 16, 16), GraphicsUnit.Pixel);
+                    //Top
+                    g.DrawImage(sampleImg, new Rectangle(16, 0, 32, 16), new Rectangle(selectRect.X + 16, selectRect.Y + 80, 32, 16), GraphicsUnit.Pixel);
+                    //Bottom
+                    g.DrawImage(sampleImg, new Rectangle(16, 48, 32, 16), new Rectangle(selectRect.X + 16, selectRect.Y + 32, 32, 16), GraphicsUnit.Pixel);
+                    //Top Right
+                    g.DrawImage(sampleImg, new Rectangle(48, 0, 16, 16), new Rectangle(selectRect.X + 32, selectRect.Y + 16, 16, 16), GraphicsUnit.Pixel);
+                    //Right
+                    g.DrawImage(sampleImg, new Rectangle(48, 16, 16, 32), new Rectangle(selectRect.X + 0, selectRect.Y + 48, 16, 32), GraphicsUnit.Pixel);
+                    //Bottom Right
+                    g.DrawImage(sampleImg, new Rectangle(48, 48, 16, 16), new Rectangle(selectRect.X + 32, selectRect.Y + 0, 16, 16), GraphicsUnit.Pixel);
+
+
+
+                    //Right Chunk
+                    g.DrawImage(sampleImg, new Rectangle(64, 0, 64, 64), new Rectangle(selectRect.X, selectRect.Y + 32, 64, 64), GraphicsUnit.Pixel);
+
+                    g.Dispose();
+                    SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                    saveFileDialog1.Filter = "Png Image|*.png";
+                    saveFileDialog1.Title = "Save Terrain Tile";
+                    saveFileDialog1.ShowDialog();
+
+                    // If the file name is not an empty string open it for saving.  
+                    if (saveFileDialog1.FileName != "")
+                    {
+                        b.Save(saveFileDialog1.FileName, ImageFormat.Png);
+                    }
                 }
             }
         }
